30e7280a9b07b0b7ff30ebb831c676da87ac260b
treby <treby@atelier-nodoka.net>
2014-03-15 11:57:10 +0900

    Added css-filter.

--- js/init.js
+++ js/init.js
@@ -4,6 +4,12 @@
   var onLoad = function() {
     var ui = new SideMMyDeskUI();
     ui.onLoad();
+
+    var elem = document.getElementById('cover-heading_input');
+    elem.addEventListener('change', function() {
+      cssFilter('#sidem-mydesk').blur(this.value + 'px');
+    });
+    cssFilter('#sidem-mydesk').blur('10px');
   };
 
   window.addEventListener('load', onLoad, false);
